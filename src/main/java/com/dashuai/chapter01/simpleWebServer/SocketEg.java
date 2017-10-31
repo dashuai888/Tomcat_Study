3dd10b3337507f79f333ba1da042832b3e5c3fa1
dashuai <yishuaiyanfu@163.com>
2017-10-31 21:52:30 +0800

    init

---
+++ src/main/java/com/dashuai/chapter01/simpleWebServer/SocketEg.java
@@ -0,0 +1,85 @@
+package com.dashuai.chapter01.simpleWebServer;
+
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStream;
+import java.io.InputStreamReader;
+import java.io.OutputStream;
+import java.io.PrintWriter;
+import java.net.Socket;
+import java.net.UnknownHostException;
+
+/**
+ * java 的 Socket 类表示一个客户端 socket，socket是网络连接的端点.
+ * <p>
+ * 通过 socket ，程序就能从网络中获取和接收数据
+ *
+ * @author wangyishuai 2017年10月29日
+ */
+public class SocketEg {
+
+    /**
+     * 一个 阻塞I/O 客户端的实现案例.
+     *
+     * @param args
+     * @throws IOException
+     * @throws UnknownHostException
+     * @throws InterruptedException
+     */
+    public static void main(String[] args) throws UnknownHostException, IOException, InterruptedException {
+        /*
+         * 创建一个socket流，同时指定该流要连接的主机（通过主机名指定的，当然也能直接指定ip地址）及其对应的端口号 这里本机测试，设置的是回环接口地址.
+         * 创建socket，下面就可以通过它来发送或者接收网络数据。
+         */
+        Socket socket = new Socket("127.0.0.1", 8111);
+        // 如果要发送文本，就需要创建一个字符输出流，但是要先建立字节输出流，因为网络数据是字节流的形式
+        OutputStream outputStream = socket.getOutputStream();
+        // 字符输出流基于上面创建的字节输出流建立，PrintWriter 可以把字节序列使用默认的编码规则格式的转化为字符序列
+        boolean autoFlush = true;
+        // 设置自动刷新为true，字符流在输出时，会把缓存的输出流刷新
+        PrintWriter printWriter = new PrintWriter(outputStream, autoFlush);
+
+        /*
+         * 要接收文本，一样的道理，也就是需要读取数据，先创建输入流
+         */
+        InputStream inputStream = socket.getInputStream();
+        // 要读取的是文本内容，故使用 InputStreamReader，它是一个将字节流转化为字符流的桥梁，它读取字节并将其解码为字符
+        InputStreamReader inputStreamReader = new InputStreamReader(inputStream);
+        // 从字符输入流中读取数据，并使用缓存 buffers 保存字符序列使其转化为文本内容
+        BufferedReader bufferedReader = new BufferedReader(inputStreamReader);
+
+        /*
+         * 模拟发送HTTP请求消息
+         *
+         * @see http.md
+         */
+        printWriter.println("GET /Tomcat_Study/src/main/java/com/dashuai/chapter01/simpleWebServer/http.md HTTP/1.1");
+        printWriter.println("Host: 127.0.0.1:8111");
+        printWriter.println("Connection: Close");
+        printWriter.println();
+
+        /*
+         * 由于是在本地请求，用的是一个socket，下面模拟读取响应消息其实就是上面的请求消息
+         */
+        StringBuilder stringBuilder = new StringBuilder();
+        boolean loop = true;
+        while (loop) {
+            // 阻塞I/O，需要等待输入流是否已经准备好被读取，如果buffer非空，那么就说明缓存的字符流已经准备好被读取了。
+            // bufferedReader.ready(),如果返回false，其实并不能保证后面会不会有流继续来，故需要循环判断
+            if (bufferedReader.ready()) {
+                int i = 0;
+                while (i != -1) {
+                    // 读取单个的字符，如果buffer中的输入流读取完毕，就返回 -1。否则返回读取的字符的int形式编码（char类型）。范围为0-65535
+                    i = bufferedReader.read();
+                    stringBuilder.append((char) i); // 转换回char
+                }
+                loop = false; // 读取完毕
+            }
+            // 为了保证读取数据完整，需要暂停当前线程，等待一段时间
+            Thread.sleep(50);
+        }
+
+        System.out.println(stringBuilder.toString());
+        socket.close();
+    }
+}
---
+++ src/main/java/com/dashuai/chapter01/simpleWebServer/servlet和catalina.md
@@ -0,0 +1,24 @@
+### tomcat 三件微小的工作：
+servlet 容器，针对一个 servlet 的 request 请求，就做了三件很微小的事:
+* 创建一个实现了 javax.servlet.ServletRequest 接口或 javax.servlet.http.ServletRequest 接口的 HttpServletRequest 类，该类实例化一个 Request 对象，并用请求参数、请求头(headers)、cookies、查询字符串、uri 等信息填充该 Request 对象
+    *  个人感觉这些东西（比如接口的名字等）没有必要记，主要是学习掌握 tomcat 这个优秀开源容器的设计思路，并能用到实际项目中。
+* 创建一个实现了 javax.servlet.ServletResponse 接口或 javax.servlet.http.ServletResponse 接口的 HttpServletResponse 类，该类实例化一个 Response 对象
+* 调用相应的 servlet 的服务方法，将先前创建的 request 对象和 response 对象作为参数传入。接收请求的 servlet 从 request 对象中读取信息，并将返回值写入到 response 对象
+    *  其实很简单的一个模型，eg；</br>
+` doSth (HttpServletRequest request, HttpServletResponse response) `
+
+<b>就是三件事，很微小的工作：一个请求处理类，一个响应处理类，一个服务类。</b>
+
+#### catalina 
+catalina 是 servlet 容器的代称，它的设计开发结构十分优雅，功能结构十分的模块化。
+
+从 servlet 容器的功能角度看，catalian 可以划分为两大模块: connector 模块和 container 模块。
+</br>
+![](/Users/wangyishuai/eclipse-workspace/Tomcat_Study/Tomcat_Study/src/main/resources/image/study/catalina结构.png)
+
+1. connector 将用户请求与 container 连接
+    1.  connector 是为每个它收到的 HTTP 请求建立 request 对象和 response 对象。
+    2.  将处理过程交给 container 模块。
+2. container 模块从 connector 模块中收到 request 对象和 response 对象，并做一些预处理工作，比如身份认证、session创建等，最后才调用相应的 servlet 服务方法
+
+##### 综上，可以知道，深入 tomcat，首先是从 connector 入手。
